215e591c34a1f03433d37d57ea1a14b01581dad5
matthew-estes <matthew.estes19@gmail.com>
2024-06-07 14:12:43 -0400

    import and include both middleware functions above all of the routes and controllers in server.js.

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const recipesController = require('./controllers/recipes.js');
