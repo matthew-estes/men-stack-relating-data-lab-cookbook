e6b6eb471db5cc167cdf724b4e4753b70098db17
matthew-estes <matthew.estes19@gmail.com>
2024-06-11 15:08:51 -0400

    create index route for Recipes

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,19 +1,23 @@
 // controllers/recipes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/user.js');
-const Recipe = require('../models/recipe.js');
+const User = require("../models/user.js");
+const Recipe = require("../models/recipe.js");
 
-router.get('/', async function(req, res) {
-    res.render('/index.ejs');
-  });
-
-router.get('/new', async function (req, res) {
-    res.render('recipes/new.ejs');
+router.get("/", async function (req, res) {
+  try {
+    const userRecipes = await Recipe.find({ owner: req.session.user._id });
+    res.render("recipes/index.ejs", { recipes: userRecipes });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
-  
+router.get("/new", async function (req, res) {
+  res.render("recipes/new.ejs");
+});
 
 module.exports = router;
